cec719796002fed8bcd5d99ef5ef354d64c03d11
Jonas <josl@dhi-gras.com>
2017-09-12 21:19:54 +0200

    update dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     url='https://www.dhi-gras.com',
     packages=find_packages(),
     install_requires=[
-        'earthdata_download'],
+        'earthdata_download>=0.5'],
     dependency_links=[
-        'https://github.com/DHI-GRAS/earthdata_download/archive/v0.4.tar.gz#egg=earthdata_download-0.4'])
+        'https://github.com/DHI-GRAS/earthdata_download/archive/v0.5.zip#egg=earthdata_download-0.5'])
