cf8231d7a520773cf80062eea0a2327f8de1dcd9
Jonas Sølvsteen <j08lue@gmail.com>
2017-10-10 11:02:32 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='modis_atm',
-    version='0.1.3',
+    version='0.2',
     description='Get atmospheric parameters from MODIS',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
